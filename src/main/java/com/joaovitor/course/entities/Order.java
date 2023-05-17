0faadf781c5be44656b46368eb6bebb2f434eb5f
Joao Vitor <jvfs12th@gmail.com>
2023-05-17 16:02:00 -0300

    Order, Instant, ISO 8601 - PARTE 2

--- src/main/java/com/joaovitor/course/entities/Order.java
+++ src/main/java/com/joaovitor/course/entities/Order.java
@@ -20,6 +20,7 @@ public class Order implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
+	
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
